46f28d5025659a7eddf5ff92e747664d0e395df8
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2021-05-31 18:54:08 +0800

    Update guess-the-number.py

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,7 +1,5 @@
 import random
-from art import logo
 
-print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 random_number = random.randint(1, 100)
@@ -32,3 +30,11 @@ while game_continues:
   elif attempts == 0 and user_guess != random_number:
     print("You've run out of guesses, you lose.")
     game_continues = False
+
+#Number Guessing Game Objectives:
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
